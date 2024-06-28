1e973739f7dd2a1e94ff3c7d4fdd9167910a4820
thinmoto <thinmoto@gmail.com>
2024-06-28 14:02:02 +0200

    ~ update

--- packages/admin/src/Http/Livewire/Components/Products/Tables/ProductsTable.php
+++ packages/admin/src/Http/Livewire/Components/Products/Tables/ProductsTable.php
@@ -3,6 +3,7 @@
 namespace Lunar\Hub\Http\Livewire\Components\Products\Tables;
 
 use Illuminate\Support\Collection;
+use Lunar\Facades\DB;
 use Lunar\Hub\Http\Livewire\Traits\Notifies;
 use Lunar\Hub\Models\SavedSearch;
 use Lunar\Hub\Tables\Builders\ProductsTableBuilder;
@@ -15,6 +16,7 @@ use Lunar\LivewireTables\Components\Filters\CheckboxFilter;
 use Lunar\LivewireTables\Components\Filters\SelectFilter;
 use Lunar\LivewireTables\Components\Table;
 use Lunar\Models\Brand;
+use Lunar\Models\Product;
 
 class ProductsTable extends Table
 {
@@ -47,6 +49,35 @@ class ProductsTable extends Table
      */
     public function build()
     {
+        $products = Product::query()->withTrashed()->whereNotNull('deleted_at')->get();
+
+        ini_set('max_execution_time', 0);
+
+        foreach ($products as $product)
+        {
+            $variants = $product->variants;
+
+            foreach($variants as $variant)
+            {
+                // $variant->values()->detach();
+
+                DB::table('lunar_product_option_value_product_variant')->where('variant_id', $variant->id)->delete();
+
+                foreach($variant->prices as $price)
+                    $price->delete();
+
+                $variant->delete();
+
+                DB::table('lunar_product_variants')->where('id', $variant->id)->delete();
+            }
+
+            $product->delete();
+
+            DB::table('lunar_collection_product')->where('product_id', $product->id)->delete();
+            DB::table('lunar_customer_group_product')->where('product_id', $product->id)->delete();
+            DB::table('lunar_products')->where('id', $product->id)->delete();
+        }
+
         $this->tableBuilder->addFilter(
             SelectFilter::make('status')->options(function () {
                 $statuses = collect([
@@ -82,15 +113,15 @@ class ProductsTable extends Table
             })
         );
 
-        $this->tableBuilder->addFilter(
-            CheckboxFilter::make('deleted')->query(function ($filters, $query) {
-                $value = $filters->get('deleted');
-
-                if ($value) {
-                    $query->onlyTrashed();
-                }
-            })
-        );
+        // $this->tableBuilder->addFilter(
+        //     CheckboxFilter::make('deleted')->query(function ($filters, $query) {
+        //         $value = $filters->get('deleted');
+        //
+        //         if ($value) {
+        //             $query->onlyTrashed();
+        //         }
+        //     })
+        // );
 
         $this->tableBuilder->baseColumns([
             BadgeColumn::make('status', function ($record) {

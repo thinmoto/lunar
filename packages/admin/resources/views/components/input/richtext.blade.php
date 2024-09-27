341bc48d9c65614e6f4f500256ef0e072760c4ad
thinmoto <thinmoto@gmail.com>
2024-09-27 10:55:55 +0200

    ~ update

--- packages/admin/resources/views/components/input/richtext.blade.php
+++ packages/admin/resources/views/components/input/richtext.blade.php
@@ -3,7 +3,22 @@
     x-data="{
         value: @entangle($attributes->wire('model')),
         init() {
-          {{ $instanceId }} = new Quill($refs.editor, {{ json_encode($options) }})
+        let options = {{ json_encode($options) }};
+
+                  const fullToolbarOptions = [
+                  [{ header: [1, 2, 3, false] }],
+                  ['bold', 'italic', 'underline'],  [{ 'align': [] }], ['link'], [{ 'color': [] }, { 'background': [] }],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],[{ 'indent': '-1'}, { 'indent': '+1' }],
+                ['clean']
+          ];
+
+          options.modules = {
+              toolbar: {
+                  container: fullToolbarOptions
+              }
+          };
+
+          const {{ $instanceId }} = new Quill($refs.editor, options)
 
           {{ $instanceId }}.on('text-change', () => {
             $dispatch('quill-input', {{ $instanceId }}.root.innerHTML)
